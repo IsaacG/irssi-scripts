170504a0b096c818756bebcc8a74995871efa00f
Isaac G <zgithub@isaacgood.com>
2011-10-05 14:33:16 -0400

    Add channotify - notify filtered by people in a channel with you

---
+++ channotify.pl
@@ -0,0 +1,33 @@
+use warnings;
+use strict;
+ 
+use Irssi;
+ 
+use vars qw($VERSION);
+our $VERSION = 1.00;
+ 
+use vars qw(%IRSSI);
+%IRSSI = (
+  name        => 'channotify',
+  authors     => 'Isaac Good',
+  contact     => 'irssi@isaacgood.com',
+  url         => 'https://www.github.com/IsaacG',
+  license     => 'Perl',
+  description => 'Filter notify list by people in a channel with you',
+);
+
+sub cmd_channotify
+{
+	for my $nick (map {$_->{nick}} (map {$_->nicks} Irssi::channels()))
+	{
+		$on{$nick} = 1
+	}
+	for my $notify (map {$_->{mask}} Irssi::Irc::notifies())
+	{
+		$care{$_} = 1 if $on{$_}
+	}
+	Irssi::print("People on your channels: " . join ", ", keys %care)
+}
+
+Irssi::command_bind("channotify", "cmd_channotify"); 
+
