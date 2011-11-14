f56cfd2b45b7347e61457d48d1fd495c93cdb56c
Isaac G <zgithub@isaacgood.com>
2011-11-14 14:47:11 -0500

    Added run.pl; A paired down version of trigger that does exec in a window on a trigger command.

---
+++ run.pl
@@ -0,0 +1,43 @@
+#
+# Run an arbitrary command
+# by Isaac Good
+#
+
+use Irssi;
+use POSIX;
+use vars qw($VERSION %IRSSI); 
+
+use Data::Dumper;
+
+my @commands = (
+    [ '!trs', '#rss-spam', '~/bin/transmission-status' ],
+    [ '!size', '#ttest', 'printf "Items in home dir: "; ls ~/ -1 | wc -l' ],
+    [ '!upt', '#ttest', 'uptime' ]
+);
+$channel = '#ttest';
+
+$VERSION = "0.01";
+%IRSSI = (
+    authors     => "Isaac Good",
+    contact     => "irssi\@isaac.otherinbox.com", 
+    name        => "run",
+    description => "Run a command via incoming triggers. Like trigger.pl with much less functionality.",
+    license     => "Public Domain",
+    changed     => "1321296864" # localtime that
+);
+
+sub event_privmsg {
+    my ($server, $data, $nick, $address) = @_;
+    my ($target, $msg) = split(/ :/, $data, 2);
+    my ($cmd) = grep { $_->[0] eq $msg } @commands;
+    return unless (defined $cmd);
+    unless (defined Irssi::window_find_item($cmd->[1])) {
+        printf("Can not find window %s for trigger %s -> %s", $cmd->[1], $cmd->[0], $cmd->[2]);
+        return;
+    }
+    Irssi::window_find_item($cmd->[1])->command('exec - -o ' . $cmd->[2]);
+}
+
+Irssi::signal_add_last("event privmsg", "event_privmsg");
+
+# vim:set ts=4 sw=4 et:
